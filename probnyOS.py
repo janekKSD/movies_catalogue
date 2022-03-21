0f2e8369f944df350d9a6c0280f7587225a5c9cd
Jan <jankowski.nml@gmail.com>
2022-03-21 03:14:39 +0100

    Delete probnyOS.py

--- probnyOS.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["API_TOKEN"] = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDc1MTkwZTg2M2JhNDU2YmNmMWQ2Y2YwMTZhMzU1ZiIsInN1YiI6IjYxZTg0Nzg5YmM4NjU3MDA0MzE3MTNiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lj9a6DFtw3zXyN2g6zRpcCwChtMtgsjoVojUSaEI_zw"
-api_key = os.environ.get("API_TOKEN")
-
-print(api_key)git
